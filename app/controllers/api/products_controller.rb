473da2a85578da19c756eb6d828a5c8f9f493042
“Videsh <videsh.patel@gmail.com>
2019-03-03 10:22:29 -0600

    Added the show file.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,7 +5,14 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def show
+    the_id = params[:id]
+    @product = Product.find_by(id: the_id)
+    render 'show.json.jbuilder'
+  end
 
-
-
+  def create
+    
+    render 'create.json.jbuilder'
+  end
 end
--- app/views/api/products/product_one.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.message @product
-# json.name @product.name
-# json.price @product.price
-# json.image_url @product.image_url
-# json.description @product.description
