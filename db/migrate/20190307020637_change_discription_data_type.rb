ff01f02820e0be5819a80d1fbf5d8a90383d1fd8
“Videsh <videsh.patel@gmail.com>
2019-03-06 20:22:30 -0600

    changed price to decimal. Changed description to text.

---
+++ db/migrate/20190307020637_change_discription_data_type.rb
@@ -0,0 +1,6 @@
+class ChangeDiscriptionDataType < ActiveRecord::Migration[5.2]
+  def change
+    change_column :products, :description, :text
+    
+  end
+end
---
+++ db/migrate/20190307021009_update_price_attribute.rb
@@ -0,0 +1,5 @@
+class UpdatePriceAttribute < ActiveRecord::Migration[5.2]
+  def change
+    change_column :products, :price, :decimal, precision: 6, scale: 2
+  end
+end
